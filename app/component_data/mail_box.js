9d358b5ac9e25db20c37289f8d396c791a750d00
Aleksey Leshko <alekseyleshko@gmail.com>
2014-04-26 17:23:21 +0400

    Fix

--- app/component_data/mail_box.js
+++ app/component_data/mail_box.js
@@ -33,35 +33,10 @@ define(
         return Mustache.render(templates.mailBox, { mail: obj });
       };
 
-      this.getSubject = function(type, relatedMailId) {
-        var relatedMail = this.attr.dataStore.mail.filter(function(each) {
-          return each.id ==  relatedMailId;
-        })[0];
-
-        var subject = relatedMail && relatedMail.subject;
-
-        var subjectLookup = {
-          newMail: this.attr.subjectHint,
-          forward: this.attr.forwardPrefix + ": " + subject,
-          reply: this.attr.replyPrefix + ": " + subject
-        }
-
-        return subjectLookup[type];
-      };
-
-      this.getRecipientId = function(type, relatedMailId) {
-        var relatedMail = (type == 'reply') && this.attr.dataStore.mail.filter(function(each) {
-          return each.id ==  relatedMailId;
-        })[0];
-
-        return relatedMail && relatedMail.contact_id || this.attr.recipientHintId;
-      };
-
       this.after("initialize", function() {
         this.on("uiMailBoxRequested", this.serveMailBox);
         this.on("uiSendRequested", this.send);
       });
     }
-
   }
 );
